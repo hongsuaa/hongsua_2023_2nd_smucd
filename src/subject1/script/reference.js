c8fd9dc36ff785dea9219232fc2b904fd046e2c9
hongsuaa <139163369+hongsuaa@users.noreply.github.com>
2023-09-29 23:24:05 +0900

    타이포 출처 추가

--- src/subject1/script/reference.js
+++ src/subject1/script/reference.js
@@ -1,8 +1,13 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
+    author: "박영수",
+    title: "암호이야기(역사 속에 숨겨진 코드)",
+    year: "2006",
+    sourceTitle: "",
+    link: "",
+    author: "Jean Pierre Jeunet",
+    title: "BigBug",
+    year: "2023",
     sourceTitle: "",
     link: "",
   },
